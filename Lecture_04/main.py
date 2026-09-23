57afbf3946f85880c41b02038d1fb4db4e939e76
giorgi-kajaia <giorgi-kajaia@yahoo.com>
2026-09-23 14:10:51 +0400

    add lecture 04

---
+++ Lecture_04/main.py
@@ -0,0 +1,13 @@
+temperature = 61
+
+if temperature <= 30:
+    print("it's not hot")
+elif temperature <= 60:
+    print("it's very hot")
+else:
+    print("it's too hot")
+    
+
+# name: str = input("Enter the name: ")
+age: int = input("Enter the age: ")
+print(age)
