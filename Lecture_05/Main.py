020ab846dd7aeeafc34a32029eeb1cc1074cd3db
giorgi-kajaia <giorgi-kajaia@yahoo.com>
2026-09-25 17:36:23 +0400

    Lecture_05

---
+++ Lecture_05/Main.py
@@ -0,0 +1,20 @@
+# count = 0
+# while count < 5:
+#     print(count)
+#     count+=1
+
+
+
+# password = ""
+# while not password:
+#     password = input("Enter the password: ")
+# print(password)
+
+for s in "python":
+    print(s)
+
+print()
+
+for i in range(50, 12, -2):
+    print(i, end=" ")
+    
